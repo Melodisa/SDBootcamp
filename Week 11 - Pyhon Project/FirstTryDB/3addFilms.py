ae4881c38dcef8319c0e0679c719eb0c118053f7
Melodisa <Melodi_isa@hotmail.com>
2024-03-04 21:02:31 +0000

    updated files

--- Week 11 - Pyhon Project/FirstTryDB/3addFilms.py
+++ Week 11 - Pyhon Project/FirstTryDB/3addFilms.py
@@ -2,16 +2,18 @@ from connect import *
 
 def insert_films():
     try:
-        sTitle = input("Enter film title: ")
-        syearReleased = input("Enter the year the film was released: ")
-        sRating = input("Enter the film rating: ")
+        fTitle = input("Enter film title: ")
+        fyearReleased = int(input("Enter the year the film was released: "))
+        fRating = input("Enter the film rating: ")
+        fduration = int(input("The duration of the film"))
+        fgenre = input("The genre of the film")
 
 
-        dbCursor.execute("INSERT INTO tblFilms VALUES(NULL,?,?,?)", (sTitle, syearReleased, sRating))
+        dbCursor.execute("INSERT INTO tblFilms VALUES(NULL,?,?,?,?,?)", (fTitle, fyearReleased, fRating, fduration, fgenre))
 
         dbCon.commit()
 
-        print(f"{sTitle} inserted in the film table ")
+        print(f"{fTitle} inserted in the film table ")
     except sql.OperationalError as e:
         print(f"failed because:  {e}")
 
@@ -21,6 +23,6 @@ def insert_films():
     except sql.Error as er:
         print(f"failed because Error:  {er}")
 if __name__ == "__main__":
-    insert_songs()  
+    insert_films()  
 
 
--- Week 11 - Pyhon Project/FirstTryDB/connect.py
+++ Week 11 - Pyhon Project/FirstTryDB/connect.py
@@ -2,7 +2,7 @@ import sqlite3 as sql # import the sqlite3 module and assigned it an alias
 
 try:
 
-    with sql.connect("FirstTryDB/Sqlconnect.DB") as dbCon: 
+    with sql.connect("Week 11 - Pyhon Project/FirstTryDB/filmflix.db") as dbCon: 
         dbCursor = dbCon.cursor()
 
 except sql.OperationalError as e:  
